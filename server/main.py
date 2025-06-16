3d549c2098252838fbbfa23d333cf80d8fcc5e89
ChandanKumar <chandanbarada727@gmail.com>
2025-06-16 08:58:09 +0000

    new api testing

--- server/main.py
+++ server/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import httpx
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+
+
+
+load_dotenv()  
+
+dogapi = os.getenv("dogapi")
 
 # Add CORS middleware
 app.add_middleware(
@@ -31,3 +40,15 @@ async def create_message(message: Message):
 @app.get("/devtest")
 async def returnDev():
     return {"message": "Hi this is for dev testing"}
+
+@app.get("/dog-breeds")
+async def get_dog_breeds():
+    url = dogapi
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": "Failed to fetch dog breeds"}
