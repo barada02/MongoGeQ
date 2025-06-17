ab55710cf547c14380f665d4291966ec3b829131
Chandan_Kumar <chandanbarada2@gmail.com>
2025-06-17 19:55:18 +0530

    working localy

--- agentServer/main.py
+++ agentServer/main.py
@@ -3,10 +3,15 @@ import os
 import uvicorn
 from google.adk.cli.fast_api import get_fast_api_app
 
+from google.adk.sessions import InMemorySessionService
+
+
 # Get the directory where main.py is located
 AGENT_DIR = os.path.dirname(os.path.abspath(__file__))
 # Example session DB URL (e.g., SQLite)
-SESSION_DB_URL = "sqlite:///./sessions.db"
+
+#SESSION_DB_URL = "sqlite:///./sessions.db"
+
 # Example allowed origins for CORS
 ALLOWED_ORIGINS = ["http://localhost", "http://localhost:8080", "*"]
 # Set web=True if you intend to serve a web interface, False otherwise
@@ -16,7 +21,7 @@ SERVE_WEB_INTERFACE = True
 # Ensure the agent directory name ('capital_agent') matches your agent folder
 app = get_fast_api_app(
     agents_dir=AGENT_DIR,
-    session_db_url=SESSION_DB_URL,
+    #session_db_uri=None,  # Use None for in-memory sessions
     allow_origins=ALLOWED_ORIGINS,
     web=SERVE_WEB_INTERFACE,
 )
